1aa5eda52392a42fa8fc5dd63ef263cf87728537
Alija Ghimire <alijaghimire27@gmail.com>
2022-02-21 19:21:57 +0545

    User model and user migration created

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,10 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->bcrypt();
+            $table->string('address');
+            $table->string('phone');
+            $table->string('role');
             $table->rememberToken();
             $table->timestamps();
         });
